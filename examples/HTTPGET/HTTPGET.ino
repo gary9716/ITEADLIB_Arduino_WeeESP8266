11b34854d65843c7c0427b7488e28aee15014134
Tsukino KanTei <r03944003@ntu.edu.tw>
2015-11-04 00:41:20 +0800

    make a new httpRequestAPI and add a basic node HTTP Server

--- examples/HTTPGET/HTTPGET.ino
+++ examples/HTTPGET/HTTPGET.ino
@@ -19,14 +19,31 @@
  * THE SOFTWARE.
  */
 
+#include <SoftwareSerial.h>
 #include "ESP8266.h"
 
-#define SSID        "ITEAD"
-#define PASSWORD    "12345678"
-#define HOST_NAME   "www.baidu.com"
-#define HOST_PORT   (80)
+#define SSID        "Lab430PrinterWifi"
+#define PASSWORD     "androidiphone"
+#define HOST_NAME   "10.4.28.9"
+#define HOST_PORT   (8080)
+#define BUFFER_SIZE (128)
 
-ESP8266 wifi(Serial1);
+enum{
+  GET_METHOD = 0,
+  POST_METHOD = 1,
+  PUT_METHOD = 2,
+  DEL_METHOD = 3
+};
+
+const char* methodNames[] = { 
+  "GET",
+  "POST",
+  "PUT",
+  "DEL"
+};
+
+SoftwareSerial wifiSerial(10,11);
+ESP8266 wifi(wifiSerial);
 
 void setup(void)
 {
@@ -36,44 +53,59 @@ void setup(void)
     Serial.print("FW Version:");
     Serial.println(wifi.getVersion().c_str());
 
-    if (wifi.setOprToStationSoftAP()) {
-        Serial.print("to station + softap ok\r\n");
+    if (wifi.setOprToStation()) {
+        Serial.print("to station ok\r\n");
     } else {
-        Serial.print("to station + softap err\r\n");
+        Serial.print("to station err\r\n");
+    }
+    while(true) {
+      if (wifi.joinAP(SSID, PASSWORD)) {
+          Serial.print("Join AP success\r\n");
+  
+          Serial.print("IP:");
+          Serial.println( wifi.getLocalIP().c_str());   
+          break;    
+      } else {
+          Serial.print("Join AP failure\r\n");
+          delay(1000);
+      }
     }
 
-    if (wifi.joinAP(SSID, PASSWORD)) {
-        Serial.print("Join AP success\r\n");
-
-        Serial.print("IP:");
-        Serial.println( wifi.getLocalIP().c_str());       
-    } else {
-        Serial.print("Join AP failure\r\n");
-    }
-    
-    if (wifi.disableMUX()) {
-        Serial.print("single ok\r\n");
-    } else {
-        Serial.print("single err\r\n");
+    while(true) {
+      if (wifi.disableMUX()) {
+          Serial.print("single ok\r\n");
+          break;
+      } else {
+          Serial.print("single err\r\n");
+          delay(2000);
+      }
     }
     
     Serial.print("setup end\r\n");
 }
- 
-void loop(void)
-{
-    uint8_t buffer[1024] = {0};
 
-    if (wifi.createTCP(HOST_NAME, HOST_PORT)) {
-        Serial.print("create tcp ok\r\n");
-    } else {
-        Serial.print("create tcp err\r\n");
+
+void httpRequest(int methodType, const char* hostName, int port, const char* reqPath) {
+    
+    while(true) {
+      //we need to establish tcp link before send out http request
+      if (wifi.createTCP(HOST_NAME, HOST_PORT)) { 
+          Serial.print("create tcp ok\r\n");
+          break;
+      } else {
+          Serial.print("create tcp err\r\n");
+          delay(500);
+      }
     }
+    
+    char buffer[BUFFER_SIZE] = {0}; //buffer too large would cause tcp failing
+    sprintf(buffer,"%s %s HTTP/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n",methodNames[methodType],reqPath,hostName);
+    wifi.send((const uint8_t*)buffer, strlen(buffer));
 
-    char *hello = "GET / HTTP/1.1\r\nHost: www.baidu.com\r\nConnection: close\r\n\r\n";
-    wifi.send((const uint8_t*)hello, strlen(hello));
-
-    uint32_t len = wifi.recv(buffer, sizeof(buffer), 10000);
+//    Serial.print("buffer:");
+//    Serial.println(buffer);
+    
+    uint32_t len = wifi.recv((uint8_t *)buffer, sizeof(buffer), 10000);
     if (len > 0) {
         Serial.print("Received:[");
         for(uint32_t i = 0; i < len; i++) {
@@ -81,14 +113,25 @@ void loop(void)
         }
         Serial.print("]\r\n");
     }
-
+    
     if (wifi.releaseTCP()) {
-        Serial.print("release tcp ok\r\n");
+      Serial.print("release tcp ok\r\n");
     } else {
-        Serial.print("release tcp err\r\n");
+      Serial.print("release tcp err\r\n");
     }
     
-    while(1);
+}
+
+
+void loop(void)
+{
+    
+    httpRequest(GET_METHOD, "10.4.28.9", 8080, "/");
+    delay(2000);
+//    while(1);
+
+    
+    
     
 }
      
